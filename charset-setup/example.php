62db7bef3f75903a1932648bf0c321339b031f8b
Banshi Bairagi <banshi.bairagi.synergytop@gmail.com>
2023-04-11 19:17:21 +0530

    Issue The Content-Type HTTP header is missing charset attribute

---
+++ charset-setup/example.php
@@ -0,0 +1,21 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+	<?php
+		/*
+		  use below function if charset is missing or meta tag
+		*/
+		header('Content-type: text/html; charset=utf-8');
+
+    ?>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" >
+</head>
+<body>
+
+</body>
+</html>
